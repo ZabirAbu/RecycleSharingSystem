7b2c25804029a21c11772eaf4ed1b88cdfa90025
set <43414852+stjkr@users.noreply.github.com>
2024-02-10 20:03:17 +0000

    Initial styling and Home page.

--- recycling/src/Components/Header.js
+++ recycling/src/Components/Header.js
@@ -2,13 +2,28 @@ import '../CSS/Header.css';
 
 
 function Header() {
-  return (
+    return (
 
-    <div className='Header'>
+        <div className='Header'>
+            <div className='logo'>
+                LOGO
+            </div>
+            <div className='nav'>
+                <a className='nav-btn'>
+                    Home
+                </a>
+                <a className='nav-btn'>
+                    Market
+                </a>
+                <a className='nav-btn'>
+                    Login
+                </a>
 
-    </div>
+            </div>
 
-  );
+        </div>
+
+    );
 }
 
 export default Header;
--- recycling/src/Pages/Home.jsx
+++ recycling/src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import '../CSS/Home.css';
 import '../CSS/index.css';
+import FrontCover from '../Assets/front-cover.png'
 
-import Header from '../Components/Header'
+import Header from '../Components/Header';
 
 
 function Home() {
@@ -9,6 +10,19 @@ function Home() {
 
     <div className='Home'>
         <Header />
+        <div className='Home-inner'>
+          <div className='Home-image'>
+            <img src={FrontCover} />
+          </div>
+          <div className='Home-text'>
+              <h1>
+                Website Name
+              </h1>
+              <text>
+                Recycle your old stuff in exchange for points.
+              </text>
+          </div>
+        </div>
 
     </div>
 
