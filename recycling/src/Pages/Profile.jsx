7b4b1244e7708f1505927113b8a058589f1c4ac6
set <43414852+stjkr@users.noreply.github.com>
2024-02-11 11:34:14 +0000

    Added profile.

--- recycling/src/Pages/Profile.jsx
+++ recycling/src/Pages/Profile.jsx
@@ -1,26 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
+import '../CSS/Profile.css';
 
 const Profile = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [points, setPoints] = useState(0); // Initialize points with 0
 
     useEffect(() => {
-        // Check if username exists in localStorage
         const username = localStorage.getItem('username');
         if (username) {
             setIsLoggedIn(true);
         }
+
+        const storedPoints = localStorage.getItem('points');
+        if (storedPoints) {
+            setPoints(parseInt(storedPoints)); // Parse stored points as integer
+        }
     }, []);
 
+    const addPoint = () => {
+        const updatedPoints = points + 1; // Increment points
+        setPoints(updatedPoints);
+        localStorage.setItem('points', updatedPoints); // Update points in localStorage
+    };
+
     const handleLogin = () => {
-        // Perform login actions here (e.g., redirect to login page)
-        // For now, just set a dummy username in localStorage
         localStorage.setItem('username', 'Team6');
+        localStorage.setItem('points', 0);
         setIsLoggedIn(true);
+        setPoints(0); // Reset points to 0 when logging in
     };
 
     const handleLogout = () => {
-        // Perform logout actions here (e.g., clear localStorage)
         localStorage.removeItem('username');
         setIsLoggedIn(false);
     };
@@ -32,14 +43,15 @@ const Profile = () => {
                 {!isLoggedIn ? (
                     <button onClick={handleLogin}>Login</button>
                 ) : (
-                    <div>
+                    <div className='Profile-unlocked'>
+                        <img src='https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg' />
                         <p>Welcome, {localStorage.getItem('username')}!</p>
+                        <p>Points: {points}</p>
                         <button onClick={handleLogout}>Logout</button>
-                        {/* Add more profile information/components here */}
+                        <button onClick={addPoint}> Add Point </button>
                     </div>
                 )}
             </div>
-
         </div>
     );
 };
