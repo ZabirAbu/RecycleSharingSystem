19bbbecbf5a14eef3bb29c88cf0b75fade0e4295
set <43414852+stjkr@users.noreply.github.com>
2024-02-11 09:17:39 +0000

    Market updated.

--- recycling/server.js
+++ recycling/server.js
@@ -31,6 +31,21 @@ app.get('/getItems', (req, res) => {
   res.json(jsonData);
 });
 
+app.get('/getItem/:id', (req, res) => {
+    const id = req.params.id
+    // Define JSON data to be returned
+    const jsonData = {
+      data: [
+        { id: 1, title: 'Item 1', content: "Item 1", image : "https://i.etsystatic.com/20931094/r/il/9bf93f/4746438886/il_fullxfull.4746438886_3mp6.jpg"},
+        { id: 2, title: 'Item 2', content: "Item 1", image : "https://i.etsystatic.com/20931094/r/il/9bf93f/4746438886/il_fullxfull.4746438886_3mp6.jpg" },
+        { id: 3, title: 'Item 3', content: "Item 1", image : "https://i.etsystatic.com/20931094/r/il/9bf93f/4746438886/il_fullxfull.4746438886_3mp6.jpg" }
+      ]
+    };
+  
+    // Send JSON response
+    res.json(jsonData);
+  });
+
 // Start the Express server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
