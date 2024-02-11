14fca582131d6d8c3b0ec0cec975edefd49b137a
set <43414852+stjkr@users.noreply.github.com>
2024-02-11 15:28:04 +0000

    Added profile etc.

--- recycling/src/Pages/Profile.jsx
+++ recycling/src/Pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
 import '../CSS/Profile.css';
+import pointIcon from '../Assets/point.png';
 import toast, { Toaster } from 'react-hot-toast';
 
 
@@ -24,18 +25,20 @@ const Profile = () => {
         const updatedPoints = points + 1; // Increment points
         setPoints(updatedPoints);
         localStorage.setItem('points', updatedPoints); // Update points in localStorage
-        toast.success("Point added!", {duration : 3000, icon: "🎯" })
+        toast.success("Point added!", { duration: 3000, icon: "🎯" })
     };
 
     const handleLogin = () => {
         localStorage.setItem('username', 'Team6');
         localStorage.setItem('points', 0);
+        toast.success("Logged in.", { duration: 3000, icon: "👋" })
         setIsLoggedIn(true);
         setPoints(0); // Reset points to 0 when logging in
     };
 
     const handleLogout = () => {
         localStorage.removeItem('username');
+        toast.success("Logged out.", { duration: 3000, icon: "👋" })
         setIsLoggedIn(false);
     };
 
@@ -45,7 +48,7 @@ const Profile = () => {
 
             {!isLoggedIn ? (
                 <div className='Profile'>
-                    <button onClick={handleLogin}>Login</button>
+                    <button className='profileClipBtn add-point' onClick={handleLogin}>Login</button>
                 </div>
             ) : (
                 <div className='Profile-unlocked'>
@@ -57,11 +60,127 @@ const Profile = () => {
                                 <text>Welcome, {localStorage.getItem('username')}!</text>
                                 <p>Points: {points}</p>
                             </div><div>
-                                <button className='add-point' onClick={addPoint}> Add Point </button>
-                                <button className='logout' onClick={handleLogout}>Logout</button>
+                                <button className='profileClipBtn add-point' onClick={addPoint}> Add Point </button>
+                                <button className='profileClipBtn logout' onClick={handleLogout}>Logout</button>
                             </div>
                         </div>
                         <div className='profile-info'>
+                            <div className='past-purchases-container'>
+                                <h1>Past Purchases</h1>
+                                <div className='past-purchases'>
+                                    <div className='purchase-item'>
+                                        <div className='pie-cont'>
+                                            <div className='pie-img'>
+                                                <img src={pointIcon} />
+                                            </div>
+                                            <div className='pie-title'>
+                                                Purchased: Vintage Clothing
+                                            </div>
+                                        </div>
+                                        <div className='pie-date'>
+                                            11/02/2024
+                                        </div>
+                                    </div>
+                                    <div className='purchase-item'>
+                                        <div className='pie-cont'>
+                                            <div className='pie-img'>
+                                                <img src={pointIcon} />
+                                            </div>
+                                            <div className='pie-title'>
+                                                Purchased: Cooking Pan
+                                            </div>
+                                        </div>
+                                        <div className='pie-date'>
+                                            11/02/2024
+                                        </div>
+                                    </div>
+                                    <div className='purchase-item'>
+                                        <div className='pie-cont'>
+                                            <div className='pie-img'>
+                                                <img src={pointIcon} />
+                                            </div>
+                                            <div className='pie-title'>
+                                                Purchased: Bracelets
+                                            </div>
+                                        </div>
+                                        <div className='pie-date'>
+                                            10/02/2024
+                                        </div>
+                                    </div>
+                                    <div className='purchase-item'>
+                                        <div className='pie-cont'>
+                                            <div className='pie-img'>
+                                                <img src={pointIcon} />
+                                            </div>
+                                            <div className='pie-title'>
+                                                Purchased: Album Covers
+                                            </div>
+                                        </div>
+                                        <div className='pie-date'>
+                                            09/02/2024
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className='past-purchases-container'>
+                                <h1>Local Leaderboard</h1>
+                                <div className='past-purchases'>
+                                    <div className='purchase-item'>
+                                        <div className='pie-cont'>
+                                            <div className='pie-img'>
+                                            <img src={pointIcon} /> 1,201
+                                            </div>
+                                            <div className='pie-title'>
+                                                John Smith
+                                            </div>
+                                        </div>
+                                        <div className='pie-date'>
+                                        🏆
+                                        </div>
+                                    </div>
+                                    <div className='purchase-item'>
+                                        <div className='pie-cont'>
+                                            <div className='pie-img'>
+                                            <img src={pointIcon} /> 1,190
+                                            </div>
+                                            <div className='pie-title'>
+                                                Jane Doe
+                                            </div>
+                                        </div>
+                                        <div className='pie-date'>
+                                            🥇
+                                        </div>
+                                    </div>
+                                    <div className='purchase-item'>
+                                        <div className='pie-cont'>
+                                            <div className='pie-img'>
+                                                <img src={pointIcon} /> 1,102
+                                            </div>
+                                            <div className='pie-title'>
+                                                Elen Joy
+                                            </div>
+                                        </div>
+                                        <div className='pie-date'>
+                                        🥈
+                                        </div>
+                                    </div>
+                                    <div className='purchase-item'>
+                                        <div className='pie-cont'>
+                                            <div className='pie-img'>
+                                                <img src={pointIcon} /> 1,009
+                                            </div>
+                                            <div className='pie-title'>
+                                                Sam Jackson
+                                            </div>
+                                        </div>
+                                        <div className='pie-date'>
+                                        🥉
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
 
                         </div>
                     </div>
